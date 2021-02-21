6036ef1bf1e88f7ad19b1f1a14987e4be867adfc
Duane Chew <duanechew@gmail.com>
2021-02-21 08:08:25 -0500

    fba7b2c

---
+++ _nuxt/static/1613782375/projects/1001/illustrations-for-encamp/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/projects/1001/illustrations-for-encamp", {data:[{}],fetch:[],mutations:void 0});
---
+++ data/projects.js
@@ -0,0 +1,86 @@
+/** Dummy user data. */
+module.exports = [
+  {
+    id: 1001,
+    slug: 'illustrations-for-encamp',
+    title: 'Illustrations for Encamp',
+    shortDescription: 'Illustrations for Encamp',
+    previewColor: 'red',
+    previewImage: '/images/pooch.jpg',
+    textA: 'A recent rebrand for Encamp resulted in a new look. SImplistic, two-tone, camping influenced illustrations were created specifically for a new website launch.',
+    textB: 'Using the outdoors visually while also including the simplistic nature of Encamp’s application was important. It was super fun to illustrate something I’m very passionate about - the healing and calming essence of the outdoors.'
+  },
+  {
+    id: 1002,
+    slug: 'holiday-illustrations-for-encamp',
+    title: 'Holiday Illustrations for Encamp',
+    shortDescription: 'Holiday Illustrations for Encamp',
+    previewColor: 'blue',
+    previewImage: '/images/pooch.jpg',
+    textA: 'These were used in social media announcements, email newsletters, and internally for the annual holiday party.',
+    textB: 'Including the outdoors and creating a magical scene to use and share across multiple platforms were a must for this past year. Sasquatch makes an appearance from time to time and Encamp’s graphics and this was no exception. '
+  },
+  {
+    id: 1003,
+    slug: 'branding-for-tooma',
+    title: 'Branding for Tooma',
+    shortDescription: 'Branding for Tooma',
+    previewColor: 'magenta',
+    previewImage: '/images/pooch.jpg',
+    textA: 'Tooma is an app that provides easy, visual website documentation for teams. I created the brand identity for the app. This included a logo, mark, color palette, type style, and homepage mock up.',
+    textB: 'Tooma was looking for a recognizable brand that was a bit quirky and also requested a dinosaur mark to represent archiving. This brand identity does just that!'
+  },
+  {
+    id: 1004,
+    slug: 'lead-acid-batteries-ebook',
+    title: 'Lead-Acid Batteries Ebook',
+    shortDescription: 'Lead-Acid Batteries Ebook',
+    previewColor: 'purple',
+    previewImage: '/images/pooch.jpg',
+    textA: 'This ebook is used as a guide to Lead-Acid Batteries. It will tell you how and why, regulatory requirements apply to lead-acid batteries. It consists of custom illustrations, copy layout, and infographics.',
+    textB: 'The ebook lives online and as an interactive pdf in hopes to guide the user easily throughout the document.'
+  },
+  {
+    id: 1005,
+    slug: 'two',
+    title: 'Two',
+    shortDescription: 'Graphic Design',
+    previewColor: 'pink',
+    previewImage: '/images/pooch.jpg'
+  },
+  {
+    id: 1006,
+    title: 'Three',
+    shortDescription: 'Brand Design',
+    previewColor: 'orange',
+    previewImage: '/images/pooch.jpg'
+  },
+  {
+    id: 1007,
+    title: 'Four',
+    shortDescription: 'Website Design',
+    previewColor: 'yellow',
+    previewImage: '/images/pooch.jpg'
+  },
+  {
+    id: 1008,
+    title: 'Five',
+    shortDescription: 'Print Design',
+    previewColor: 'cyan',
+    previewImage: '/images/pooch.jpg'
+  },
+  { 
+    id: 1009,
+    title: 'Six',
+    shortDescription: 'Project Manager',
+    previewColor: 'green',
+    previewImage: '/images/pooch.jpg'
+  },
+  {
+    id: 1010,
+    title: 'Seven',
+    shortDescription: 'Ladie Dah',
+    previewColor: 'grey',
+    previewImage: '/images/pooch.jpg'
+  },
+];
