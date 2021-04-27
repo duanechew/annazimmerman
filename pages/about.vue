bf4b9d06fd2130f28e7e7ab182e9fa4dc4487d1b
Duane Chew <duanechew@gmail.com>
2021-04-26 20:34:32 -0400

    add project pages + about

---
+++ pages/about.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="wrap1280">
+    <Breadcrumbs :links="[{ title: 'About', href: '/about' }]" />
+    <div class="outer">
+      <div class="upper">
+        <div>
+          <img width="100%" :src="`${imageUrl}/pooch.jpg`" />
+        </div>
+        <div>
+          <div class="upper-headline">HELLO</div>
+          <div class="upper-heavy heavy">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
+            sed diam nonummy nibh euismod tincidunt ut laoreet dolore 
+            magna
+          </div>
+          <div class="light">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+            sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+            magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+            quis nostrud exerci tation ullamcorper suscipit lobortis
+            nisl ut aliquip ex
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="lower">
+      <div class="lower-left">
+        <div class="heavy">
+          An illustrative designer, a designy illustrator
+        </div>
+        <div class="light lower-light">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+          sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+          magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+          quis nostrud exerci tation ullamcorper suscipit lobortis
+          nisl ut aliquip ex
+        </div>
+        <Button buttonHref="home" buttonText="Download My Resume" />
+      </div>
+      <div class="lower-right">
+        <div class="light">
+          Illustration <br />
+          Style Development, Product Illustration, Icons, Editorial
+        </div>
+        <div class="light">
+          Brand Design <br />
+          Brand Development, Brand Campaigns, Social Campaigns
+        </div>
+        <div class="light">
+          Visual Design <br />
+          UI, End-to-End Product Design, Style Guide Development
+        </div>
+      </div>
+    </div>
+
+    <CTAWithImage
+      :image="'/images/pooch.jpg'"
+      :body="'Interested in working together? Want to learn more about the projects above?'"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.outer {
+  display: flex;
+}
+
+.heavy {
+  font-size: 1.5rem;
+  font-weight: 800;
+}
+
+.light {
+  font-size: 1.3rem;
+}
+
+.upper {
+  padding-bottom: 90px;
+  margin-bottom: 90px;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: solid 1px white;
+  /* align-items: center; */
+  div {
+    flex-basis: 48%;
+  }
+}
+
+.upper-headline {
+  font-size: 2.5rem;
+  color: #c76253;
+  font-weight: 800;
+}
+
+.upper-heavy {
+  padding: 40px 0;
+}
+
+.lower {
+  display: flex;
+  justify-content: space-between;
+}
+
+.lower-left { flex-basis: 60%; }
+
+.lower-right {
+  flex-basis: 38%;
+  line-height: 2rem;
+  div {
+    padding-bottom: 30px;
+    &:last-child {
+      padding-bottom: 0;
+    }
+  }
+}
+
+.lower-light { padding: 40px 0;
+}
+</style>
---
+++ pages/projects/_id/_slug.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="wrapper">
+    <Breadcrumbs :links="[{ title:this.project.title, href: `/projects/${this.project.id}/${this.project.slug}` }]" />
+    <div class="full-width-img">
+      <img width="100%" :src="`${$config.baseUrl}${this.project.topImage}`" />
+    </div>
+    <div class="bg-txt">
+      {{ this.project.textA }}
+    </div>
+    <div class="img-cols">
+      <div>
+        <!-- <img width="100%" :src="`${$config.baseUrl}${this.project.bottomImages[2]}`" /> -->
+      </div>
+      <div>
+        <!-- <img width="100%" :src="`${$config.baseUrl}${this.project.bottomImages[1]}`" /> -->
+      </div>
+    </div>
+    <div class="bg-txt">
+      {{ this.project.textB }}
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+const projects = require('@/static/data/projects')
+
+export default Vue.extend({
+  data() {
+    return {
+      project: {}
+    }
+  },
+  created() {
+    projects.forEach((project) => {
+      if (project.id.toString() === this.$route.params.id) {
+        this.project = project;
+      }
+    })
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  max-width: 1200px;
+  padding: 0 20px;
+  margin: 0 auto;
+}
+.full-width-img {
+  width: 100%;
+}
+.bg-txt {
+  font-size: 2rem;
+  padding: 5rem 0;
+}
+.img-cols {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  div {
+    flex-basis: 48%;
+  }
+}
+</style>
+
